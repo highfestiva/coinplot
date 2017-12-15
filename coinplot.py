0581e690cb27778e4d8452a5ad360a9b9bcb0e9e
Jonas <jonas.bystrom@hm.com>
2017-12-15 14:13:04 +0100

    Add caching and line plotting

--- coinplot.py
+++ coinplot.py
@@ -6,11 +6,13 @@ from bokeh.plotting import figure
 from bokeh.models import WheelZoomTool
 from bokeh.models.formatters import DatetimeTickFormatter
 from flask import Flask, request, render_template
+from flask_caching import Cache
 import pandas as pd
 import requests
 
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 @app.route('/')
@@ -25,6 +27,8 @@ def currency(currency='BTC_USDT', to_currency='USDT'):
     interval = request.values.get('interval', '15m')
     log = request.values.get('log', 'false')
     log = log in ('true', '1', 'on', 'yes', 'y')
+    line = request.values.get('line', 'false')
+    line = line in ('true', '1', 'on', 'yes', 'y')
     url = 'https://www.binance.com/api/v1/klines?interval={interval}&symbol={symbol}'.format(interval=interval, symbol=symbol)
     df = read_frame(url)
     if in_cur != to_currency and from_cur != to_currency:
@@ -54,9 +58,11 @@ def currency(currency='BTC_USDT', to_currency='USDT'):
         to_currency = in_cur
 
     title = from_cur + '/' + to_currency
-    return plot2html(df, title, interval, log)
+    return plot2html(df, title, interval, log, line)
 
 
+#@app.route('/cache/<url>')
+@cache.cached(timeout=7*60)
 def read_frame(url):
     print(url)
     data = requests.get(url).json()
@@ -70,7 +76,7 @@ def read_frame(url):
     return df
 
 
-def plot2html(df, title, interval, log):
+def plot2html(df, title, interval, log, line):
     df = df.iloc[1:]
     up = df.close > df.open
     dn = df.open  > df.close
@@ -106,14 +112,21 @@ def plot2html(df, title, interval, log):
     dtf.years = ['%F']
     plot.xaxis.formatter = dtf
 
-    totime = {'1m':60, '5m':5*60, '15m':15*60, '30m':30*60, '1h':60*60, '2h':2*60*60, '4h':4*60*60, '6h':6*60*60, '12h':12*60*60, '1d':24*60*60}
-    w = totime[interval] * 0.7 * 1000
+    if line:
+        plot.line(df.time, df.close, color='#55bb77')
+    else:
+        pass
+
     plot.line(df.time, ma12, color='#ffee33', legend='ma-12')
     plot.line(df.time, ma26, color='#3355ff', legend='ma-26')
-    plot.segment(df.time[up], df.hi[up], df.time[up], df.lo[up], color='#33dd99')
-    plot.segment(df.time[dn], df.hi[dn], df.time[dn], df.lo[dn], color='#ff8866')
-    plot.vbar(df.time[up], w, df.open[up], df.close[up], fill_color='#558866', line_color='#33dd99')
-    plot.vbar(df.time[dn], w, df.open[dn], df.close[dn], fill_color='#cc9988', line_color='#ff8866')
+
+    if not line:
+        totime = {'1m':60, '5m':5*60, '15m':15*60, '30m':30*60, '1h':60*60, '2h':2*60*60, '4h':4*60*60, '6h':6*60*60, '12h':12*60*60, '1d':24*60*60}
+        w = totime[interval] * 0.7 * 1000
+        plot.segment(df.time[up], df.hi[up], df.time[up], df.lo[up], color='#33dd99')
+        plot.segment(df.time[dn], df.hi[dn], df.time[dn], df.lo[dn], color='#ff8866')
+        plot.vbar(df.time[up], w, df.open[up], df.close[up], fill_color='#558866', line_color='#33dd99')
+        plot.vbar(df.time[dn], w, df.open[dn], df.close[dn], fill_color='#cc9988', line_color='#ff8866')
 
     plot.legend.background_fill_color = '#222222'
     plot.legend.background_fill_alpha = 0.6
